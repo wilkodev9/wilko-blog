1046adfd593734d9ead10f5c3acf7e704ef579bf
wilkodev9 <wilkodeveloper@gmail.com>
2023-04-08 13:07:29 +0200

    html setup for the blog/"id" page

--- portfolio-blog/pages/blog/id.tsx
+++ portfolio-blog/pages/blog/id.tsx
@@ -4,6 +4,25 @@ export default function Id() {
     return (
     <div>
       <Navbar></Navbar>
+      <header>
+        <h2>the blog tittle and description</h2>
+      </header>
+      <h3>
+        this is the tittle of this paragraph
+      </h3>
+      <p>
+        this blog is about creating some stuff
+      </p>
+      <div>
+        <h2>
+          authort
+        </h2>
+        <p>
+          wilko
+        </p>
+        <p>tags</p>
+        <p>socials</p>
+      </div>
     </div>
     )
   }
